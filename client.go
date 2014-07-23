5d490c127af3eefde4f32f3df1585c0f61291f45
user <johnnydiabetic@gmail.com>
2014-07-23 10:43:31 -0400

    Moving to other computer

--- client.go
+++ client.go
@@ -28,16 +28,16 @@ func GetResponse() {
 }
 
 func main() {
-	//	GetResponse()
-	rc := retcalc.NewRetCalc_b()
-	rc.Yearly_retirement_expenses = 60000.0
-	rc.Non_Taxable_contribution = 0.0
-	rc.Non_Taxable_balance = 0.0
-	rc.Taxable_contribution = 17500.0
-	rc.Taxable_balance = 0.0
+	JsonObj := []byte(`{"Age":22, "Retirement_age":65, "Terminal_age":90, "Effective_tax_rate":0.3, "Returns_tax_rate":0.3, "N": 20000, 
+						"Non_Taxable_contribution":17500, "Taxable_contribution": 0, "Non_Taxable_balance":0, "Taxable_balance": 0, 
+						"Yearly_social_security_income":0, "Asset_volatility": 0.15, "Expected_rate_of_return": 0.07, "Inflation_rate":0.035}`)
+	rc := retcalc.NewRetCalc_from_json(JsonObj)
+
+	s := make([]float64, rc.Years, rc.Years)
+	for i := range s {
+		s[i] = 0.07
+	}
+	rc.SetSim(0, s)
 	rc.All_paths = rc.RunAllPaths()
-	//fmt.Println(rc.RunIncomes())
-	fmt.Println("\n")
-	fmt.Println(rc.IncomeProbability())
-	fmt.Println(rc.PercentileIncome(0.2))
+	fmt.Println(rc.IncomeOnPath(0))
 }
