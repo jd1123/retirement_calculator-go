9bb5e75ece4c75dea13f352753e97a621ce75744
johnnydiabetic <johnnydiabetic@gmail.com>
2014-08-06 22:10:11 -0400

    Added clean up

--- main.go
+++ main.go
@@ -24,14 +24,29 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"retirement_calculator-go/server"
+	"runtime/pprof"
 	"strconv"
 )
 
 const listenPort = 8081
 
 func main() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for sig := range c {
+			log.Printf("captured %v cleaning up and exiting..", sig)
+			os.Remove("tmp/myfile")
+			pprof.StopCPUProfile()
+			os.Exit(1)
+		}
+	}()
+
 	listenStr := ":" + strconv.Itoa(listenPort)
 	server.RegisterHandlers()
 	http.Handle("/", http.FileServer(http.Dir("./static")))
