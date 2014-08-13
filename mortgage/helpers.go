87fcc90cf879dc8a51b2c93a53a4f43f1b53f605
user <johnnydiabetic@gmail.com>
2014-08-12 20:58:17 -0400

    Added some more helper functions in mortgage

--- mortgage/helpers.go
+++ mortgage/helpers.go
@@ -9,17 +9,37 @@ func MortgageMonthlyPayment(loanAmount, r float64, termInMonths int) float64 {
 }
 
 func MonthlyMoneyGrowthVector(amount, r float64, termInMonths int) []float64 {
-	c := r / 12.0
 	out := make([]float64, termInMonths, termInMonths)
 	for i := 0; i < termInMonths; i++ {
-		out[i] = amount * math.Pow((1+c), float64(i))
+		out[i] = amount * GF_m(r, i) //math.Pow((1+c), float64(i))
 	}
 	return out
 }
 
 func MonthlyMoneyGrowthTerminal(amount, r float64, termInMonths int) float64 {
-	c := r / 12.0
-	return amount * math.Pow((1+c), float64(termInMonths))
+	return amount * GF_m(r, termInMonths) // math.Pow((1+c), float64(termInMonths))
+}
+
+func SumProduct(a, b []float64) float64 {
+	if len(a) != len(b) {
+		panic("lengths of slices are not equal!")
+	}
+	sum := 0.0
+	for i := 0; i < len(a); i++ {
+		sum += a[i] * b[i]
+	}
+	return sum / float64(len(a))
+}
+
+// Growth factor and discount factor functions
+func DF_d(r float64, termInDays int) float64 {
+	c := r / 365.25
+	return 1 / math.Pow((1+c), float64(termInDays))
+}
+
+func GF_d(r float64, termInDays int) float64 {
+	c := r / 365.25
+	return 1 / math.Pow((1+c), float64(termInDays))
 }
 
 func DF_m(r float64, termInMonths int) float64 {
